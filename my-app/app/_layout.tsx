93f8571ece44e5bc87f590c94b7c33ffc1d78d7e
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-03-17 22:44:35 +0530

    navigation bug fixed

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,10 +1,20 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
   return (
     <GestureHandlerRootView>
-      <Slot />
+      {/* <Slot /> */}
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="(nobottombar)/accountinfo"
+          options={{
+            headerShown: true,
+            headerTitle: "Account info",
+            headerBackTitle: "go back",
+          }}
+        />
+      </Stack>
     </GestureHandlerRootView>
   );
 }
